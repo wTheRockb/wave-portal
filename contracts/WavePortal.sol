583d2bedaa8d53eb2ca2cb3dc96a5270296b4293
wtherockb <forrestwbutler@gmail.com>
2021-10-21 14:34:13 -0700

    Change cooldown period to 30s

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -33,8 +33,8 @@ contract WavePortal {
          * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
          */
         require(
-            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
-            "Wait 15m"
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 30s"
         );
 
         /*
